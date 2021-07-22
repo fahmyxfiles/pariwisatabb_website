917be56b93122f49189164252a8d2c4ac9e5fbeb
fahmyxfiles <fahmy.xfiles@gmail.com>
2021-07-22 21:08:26 +0800

    updated

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -4,5 +4,4 @@
 <script src="{{ url('/js/jquery.mousewheel.min.js') }}"></script>
 <script src="{{ url('/js/slick.min.js') }}"></script>
 <script src="{{ url('/js/hammer.js') }}"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="{{ url('/js/scripts.js') }}"></script>
--- resources/views/layouts/styles.blade.php
+++ resources/views/layouts/styles.blade.php
@@ -1,6 +1,5 @@
 <link href="https://fonts.googleapis.com/css?family=Nunito:400,700&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Prata&display=swap" rel="stylesheet">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <link rel="stylesheet" href="{{ url('/css/style.css') }}">
 <link rel="apple-touch-icon" sizes="180x180" href="{{ url('/img/favicons/apple-touch-icon.png') }}">
 <link rel="icon" type="image/png" sizes="32x32" href="{{ url('img/favicons/favicon-32x32') }}">

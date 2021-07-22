69717e269ffb44fe10497a382c3c41e700d7ffd4
fahmyxfiles <fahmy.xfiles@gmail.com>
2021-07-22 17:36:00 +0800

    Update 2021_07_22_092420_create_event_files_table.php

--- database/migrations/2021_07_22_092420_create_event_files_table.php
+++ database/migrations/2021_07_22_092420_create_event_files_table.php
@@ -16,7 +16,6 @@ class CreateEventFilesTable extends Migration
         Schema::create('event_files', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('event_id');
-            $table->string('size');
             $table->string('filename');
             $table->string('mimes');
             $table->string('size');

3dedd133b7280d7fc70964563135f87f0fb09d20
fahmyxfiles <fahmy.xfiles@gmail.com>
2021-10-20 10:48:17 +0700

    updated

--- resources/views/admin/layouts/sidenav.blade.php
+++ resources/views/admin/layouts/sidenav.blade.php
@@ -3,8 +3,8 @@
     <div class="scrollbar-inner">
       <!-- Brand -->
       <div class="sidenav-header d-flex align-items-center">
-        <a class="navbar-brand" href="../../pages/dashboards/dashboard.html">
-          <img src="../../assets/img/brand/blue.png" class="navbar-brand-img" alt="...">
+        <a class="navbar-brand" href="/admin">
+          <img src="{{ asset('img/brand/blue.png') }}" class="navbar-brand-img" alt="...">
         </a>
         <div class="ml-auto">
           <!-- Sidenav toggler -->

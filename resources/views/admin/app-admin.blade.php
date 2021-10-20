0ea9e0c707f791a9ae5b466e49e9c205411aac9a
fahmyxfiles <fahmy.xfiles@gmail.com>
2021-10-20 10:45:26 +0700

    Update app-admin.blade.php

--- resources/views/admin/app-admin.blade.php
+++ resources/views/admin/app-admin.blade.php
@@ -21,15 +21,15 @@
    <meta name="author" content="Creative Tim">
    <title>Argon Dashboard PRO - Premium Bootstrap 4 Admin Template</title>
    <!-- Favicon -->
-   <link rel="icon" href="{{ asset('assets/img/brand/favicon.png') }}" type="image/png">
+   <link rel="icon" href="{{ asset('img/brand/favicon.png') }}" type="image/png">
    <!-- Fonts -->
    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
    <!-- Icons -->
-   <link rel="stylesheet" href="{{ asset('assets/vendor/nucleo/css/nucleo.css') }}" type="text/css">
-   <link rel="stylesheet" href="{{ asset('assets/vendor/@fortawesome/fontawesome-free/css/all.min.css') }}" type="text/css">
+   <link rel="stylesheet" href="{{ asset('vendor/nucleo/css/nucleo.css') }}" type="text/css">
+   <link rel="stylesheet" href="{{ asset('vendor/@fortawesome/fontawesome-free/css/all.min.css') }}" type="text/css">
    <!-- Page plugins -->
    <!-- Argon CSS -->
-   <link rel="stylesheet" href="{{ asset('assets/css/argon.css?v=1.1.0') }}" type="text/css">
+   <link rel="stylesheet" href="{{ asset('css/argon.css?v=1.1.0') }}" type="text/css">
  </head>
  
  <body>
@@ -52,18 +52,18 @@
    </div>
    <!-- Argon Scripts -->
    <!-- Core -->
-   <script src="{{ asset('assets/vendor/jquery/dist/jquery.min.js') }}"></script>
-   <script src="{{ asset('assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
-   <script src="{{ asset('assets/vendor/js-cookie/js.cookie.js') }}"></script>
-   <script src="{{ asset('assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js') }}"></script>
-   <script src="{{ asset('assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js') }}"></script>
+   <script src="{{ asset('vendor/jquery/dist/jquery.min.js') }}"></script>
+   <script src="{{ asset('vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+   <script src="{{ asset('vendor/js-cookie/js.cookie.js') }}"></script>
+   <script src="{{ asset('vendor/jquery.scrollbar/jquery.scrollbar.min.js') }}"></script>
+   <script src="{{ asset('vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js') }}"></script>
    <!-- Optional JS -->
-   <script src="{{ asset('assets/vendor/chart.js/dist/Chart.min.js') }}"></script>
-   <script src="{{ asset('assets/vendor/chart.js/dist/Chart.extension.js') }}"></script>
+   <script src="{{ asset('vendor/chart.js/dist/Chart.min.js') }}"></script>
+   <script src="{{ asset('vendor/chart.js/dist/Chart.extension.js') }}"></script>
    <!-- Argon JS -->
-   <script src="{{ asset('assets/js/argon.js?v=1.1.0') }}"></script>
+   <script src="{{ asset('js/argon.js?v=1.1.0') }}"></script>
    <!-- Demo JS - remove this in your project -->
-   <script src="{{ asset('assets/js/demo.min.js') }}"></script>
+   <script src="{{ asset('js/demo.min.js') }}"></script>
  </body>
  
  </html>
